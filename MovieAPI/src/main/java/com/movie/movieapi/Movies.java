18edfc42afdda947fc087875284c73e83aaa577a
Clancy Thomas <clancyvthomas@gmail.com>
2022-11-08 14:24:16 -0700

    Movie - Movies.java : Added a few comments

--- MovieAPI/src/main/java/com/movie/movieapi/Movies.java
+++ MovieAPI/src/main/java/com/movie/movieapi/Movies.java
@@ -18,14 +18,17 @@ public class Movies {
         return this.movieList;
     }
 
+    // Set the movie list
     public void setMovieList(List<Movie> movieList) {
         this.movieList = movieList;
     }
 
+    // Add a movie to the list
     public void addMovie(Movie newMovie) {
         this.movieList.add(newMovie);
     }
 
+    // Remove a movie from the list
     public void removeMovie(Movie oldMovie) {
         this.movieList.remove(oldMovie);
     }
