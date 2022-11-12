509d4cd725c195becc022350c72bdc62cdaf2c99
Clancy Thomas <clancyvthomas@gmail.com>
2022-11-11 18:55:51 -0700

    Added a mysql database project to the repository

---
+++ mysqldatabaseconnection/src/main/java/com/example/mysqldatabaseconnection/MysqldatabaseconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqldatabaseconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqldatabaseconnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqldatabaseconnectionApplication.class, args);
+    }
+
+}
