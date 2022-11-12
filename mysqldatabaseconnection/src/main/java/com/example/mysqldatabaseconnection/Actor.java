01faecbda399ed19fbedb95ef438158596ee989c
Clancy Thomas <clancyvthomas@gmail.com>
2022-11-11 19:05:10 -0700

    Mysql - Actor.java : Added an actor entity

---
+++ mysqldatabaseconnection/src/main/java/com/example/mysqldatabaseconnection/Actor.java
@@ -0,0 +1,47 @@
+package com.example.mysqldatabaseconnection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String first_name;
+    private String last_name;
+
+    public Actor() {
+        this.first_name = "";
+        this.last_name = "";
+    }
+
+    public Actor(String first_name, String last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
